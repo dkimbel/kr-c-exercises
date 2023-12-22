232d46c2cee9511187c6922e1309c7398e7a3b00
Daniel Kimbel <dk@danielkimbel.com>
2023-12-21 17:23:05 -0700

    Use symbolic constants as in book

--- Ch1/05-reverse-order-temp.c
+++ Ch1/05-reverse-order-temp.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 
+#define UPPER 300
+#define LOWER 0
+#define STEP 20
+
 int main(void)
 {
   int fahr;
   float celsius;
 
   printf("%2sF\t%5sC\n", " ", " ");
-  for (fahr = 300; fahr >= 0; fahr = fahr - 20) {
+  for (fahr = UPPER; fahr >= LOWER; fahr = fahr - STEP) {
     celsius = (5.0 / 9.0) * (fahr - 32);
     printf("%3d\t%6.1f\n", fahr, celsius);
   }
